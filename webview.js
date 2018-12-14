443f978d7f5ebe0de10a2b6c8a285c7bc528fd09
Chris Romp <chris.romp@r3interworks.com>
2018-12-14 10:10:38 -0800

    Add selector for "new OWA".

--- webview.js
+++ webview.js
@@ -1,7 +1,13 @@
 module.exports = (Franz) => {
     const getMessages = function getMessages() {
+        // Legacy OWA
         const unreadMail = jQuery("span[title*='Inbox'] + div > span").first().text();
-        Franz.setBadge(unreadMail);
+
+        // "New" OWA (late 2018)
+        const unreadMailNew = parseInt(jQuery("div[title='Inbox'][role='treeitem']").children("span:contains('unread')").text().replace("unread", ""), 10);
+
+        const messageCount = isNaN(unreadMail) ? unreadMailNew : unreadMail;
+        Franz.setBadge(messageCount);
     };
     Franz.loop(getMessages);
 };
